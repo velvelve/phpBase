467c1957194cc5dc29b42f3368f13387a9d22697
Sandroisu <sandroisu1411@gmail.com>
2023-01-26 23:16:46 +0300

    done

--- script.php
+++ script.php
@@ -1,2 +1,16 @@
 <?php
-echo("hello");
+    $name = trim(readline("What is your name?\n"));
+    $tasks = array();
+    $total_time = 0;
+    for ($i = 1; $i <= 3; $i++) {
+        $task = trim(readline("What is your $i challenge for today?\n"));
+        $time = trim(readline("How long will this task take?\n"));
+        $total_time += $time;
+        $tasks[] = ["task" => $task, "time" => $time];
+    }
+    echo "$name, today you have 3 priority tasks planned for the day:\n";
+    foreach ($tasks as $task) {
+        echo "- " . $task["task"] . " (" . $task["time"] . "h)\n";
+    }
+    echo "Estimated time to complete the plan = $total_time h\n";
+?>
